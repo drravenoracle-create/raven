c75fcdbd633554feb5e1cb0c08d1232eae01b62e
Raven Oracle <dr.ravenoracle@gmail.com>
2026-08-11 12:11:49 +0900

    Enlarge reel captions and add CTA

--- scripts/render-reel.mjs
+++ scripts/render-reel.mjs
@@ -18,9 +18,10 @@ function escapeAss(text) {
 
 const input = resolve(arg("input"));
 const output = resolve(arg("output"));
-const title = escapeAss(arg("title", "答えを急がない夜"));
-const subtitle = escapeAss(arg("subtitle", "気持ちを整え、一手を選ぶ"));
+const title = escapeAss(arg("title", "\u7b54\u3048\u3092\u6025\u304c\u306a\u3044\u591c"));
+const subtitle = escapeAss(arg("subtitle", "\u6c17\u6301\u3061\u3092\u6574\u3048\u3001\u4e00\u624b\u3092\u9078\u3076"));
 const brand = escapeAss(arg("brand", "Raven Blackwood"));
+const cta = escapeAss(arg("cta", "\u8a73\u3057\u3044\u9451\u5b9a\u306f\u30d7\u30ed\u30d5\u30a3\u30fc\u30eb\u304b\u3089"));
 
 const workDir = mkdtempSync(join(tmpdir(), "raven-reel-"));
 const assPath = join(workDir, "caption.ass");
@@ -34,14 +35,16 @@ ScaledBorderAndShadow: yes
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Title,Noto Sans CJK JP,82,&H00FFFFFF,&H00FFFFFF,&HAA000000,&H99000000,-1,0,0,0,100,100,0,0,3,4,0,5,80,80,0,1
-Style: Sub,Noto Sans CJK JP,48,&H00FFFFFF,&H00FFFFFF,&HAA000000,&H88000000,0,0,0,0,100,100,0,0,3,3,0,5,80,80,0,1
+Style: Title,Noto Sans CJK JP,116,&H00FFFFFF,&H00FFFFFF,&HAA000000,&H99000000,-1,0,0,0,100,100,0,0,3,5,0,5,44,44,0,1
+Style: Sub,Noto Sans CJK JP,66,&H00FFFFFF,&H00FFFFFF,&HAA000000,&H88000000,0,0,0,0,100,100,0,0,3,4,0,5,56,56,0,1
+Style: Cta,Noto Sans CJK JP,70,&H00FFFFFF,&H00FFFFFF,&H88000000,&HAA000000,-1,0,0,0,100,100,0,0,3,4,0,2,56,56,330,1
 Style: Brand,Noto Sans CJK JP,42,&H00FFFFFF,&H00FFFFFF,&H66000000,&H00000000,-1,0,0,0,100,100,0,0,1,2,0,2,80,80,180,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
 Dialogue: 0,0:00:00.00,0:01:00.00,Title,,0,0,0,,${title}
 Dialogue: 0,0:00:00.70,0:01:00.00,Sub,,0,0,0,,${subtitle}
+Dialogue: 1,0:00:07.50,0:01:00.00,Cta,,0,0,0,,${cta}
 Dialogue: 0,0:00:00.00,0:01:00.00,Brand,,0,0,0,,${brand}
 `, "utf8");
 
